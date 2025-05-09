34871811701497d14c99b0f210b083232abc72f0
kostyaslob <kostyaslob@gmail.com>
2025-05-09 17:30:04 +0300

    add Components jsx and css files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import css from "./App.module.css";
-import { useState, useEffect } from 'react';
-import { useDebounce } from "use-debounce";
+// import { useState, useEffect } from 'react';
+// import { useDebounce } from "use-debounce";
+// import axios from "axios";
+// import { ClipLoader } from "react-spinners";
+
 
 
 export default function App() {
@@ -9,6 +12,13 @@ export default function App() {
   return (
     <div className={css.container}>
       <ContactForm />
+      <ErrorMessage />
+      <ImageGallery />
+      <ImageModal />
+      <Loader />
+      <LoadMoreBtn />
+      <SearchBar />   
+
     </div>
   );
 };
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,5 @@
+// import css from "./ImageGallery.module.css"
+// import ImageCard from "./ImageCard/ImageCard"
+
+
+// <ImageCard />
